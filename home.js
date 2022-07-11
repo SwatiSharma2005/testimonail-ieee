4318e6ac48398aa34ac0d493d78e48ae976346fd
SwatiSharma2005 <97469594+SwatiSharma2005@users.noreply.github.com>
2022-07-11 21:53:45 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+class Home extends React.Component{
+    render(){
+        return(
+            
+        <div>
+            <div class="testi">
+        <h5>TESTIMONIALS</h5>
+    </div>
+    
+        <div class="feedback">
+            <h2>What Our Clients Are Saying</h2>
+        </div>
+<div class="container">
+    <div class="qucontainer">
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <div class="pe"><p>A digital agency is a business you hire to outsource your digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you.</p></div>
+        <img  class="circular_image" src="pikachu.png"/>
+        <div class="name"><h4>Alan Martí</h4></div>
+        
+        <div class="pr"><h6>Meta Inc.</h6></div>
+        
+        
+        </div>
+        <div class="qucontainer2">
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <div class="pe">
+                <p>A digital agency is a business you hire to outsource your digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you.</p>
+            </div>
+            <img  class="circular_image" src="pikachu.png"/>
+            <div class="name"><h4>Alan Martí</h4></div>
+            <div class="pr"><h6>Meta Inc.</h6></div>
+            
+            </div>
+            
+    </div>
+    <div class="container1">
+        <div class="qucontainer1">
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <div class="pe"><p>A digital agency is a business you hire to outsource your digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you.</p></div>
+
+            <img  class="circular_image" src="pikachu.png"/>
+            <div class="name"><h4>Alan Martí</h4></div>
+            <div class="pr"><h6>Meta Inc.</h6></div>
+            
+            </div>
+            <div class="qucontainer1">
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <div class="pe"><p>A digital agency is a business you hire to outsource your digital marketing efforts, instead of handling in-house. They can provide your business with a variety of digital solutions to promote your product or service online and help you.</p></div>
+                <img  class="circular_image" src="pikachu.png"/>
+                <div class="name"><h4>Alan Martí</h4></div>
+                <div class="pr"><h6>Meta Inc.</h6></div>
+            
+                </div>
+               
+
+    </div>
+    <div class="btn"><button class="button">See All</button></div>
+        </div>
+        )
+    }
+    
+}
+
+export default Home;
